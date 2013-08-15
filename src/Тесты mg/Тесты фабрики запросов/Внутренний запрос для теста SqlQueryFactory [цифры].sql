1ed47691fd6d754bb1b7d9bb323604bbab3c4949
mg <mg@altsoft.biz>
2013-08-15 14:21:21 +0400

    bb

--- src/Тесты mg/Тесты фабрики запросов/Внутренний запрос для теста SqlQueryFactory [цифры].sql
+++ src/Тесты mg/Тесты фабрики запросов/Внутренний запрос для теста SqlQueryFactory [цифры].sql
@@ -1,6 +1,6 @@
 /**
  * @name 128082898425059
-*/
+*/ 
 Select goodOrder.ORDER_ID as ORDER_NO, goodOrder.AMOUNT, customers.CUSTOMER_NAME as CUSTOMER 
 From GOODORDER goodOrder
  Inner Join CUSTOMER customers on (goodOrder.CUSTOMER = customers.CUSTOMER_ID)
