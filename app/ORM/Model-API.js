bc8d04067ab58bb4ada7254256a90b29e48265e7
mg <mg@altsoft.biz>
2014-06-26 17:26:43 +0400

    Further model api tests

--- app/ORM/Model-API.js
+++ app/ORM/Model-API.js
@@ -107,9 +107,10 @@ function ModelAPI() {
         P.Logger.info("model.testData.unshift: " + model.testData.unshift);
         P.Logger.info("model.testData instanceof Array: " + (model.testData instanceof Array));
         P.Logger.info("model.testData instanceof P.ApplicationDbEntity: " + (model.testData instanceof P.ApplicationDbEntity));
+        model.testData.schema.MDENT_ID.pk = true;
     }
 
-    function testScroll() {
+    function testScroll(expectData) {
         // Scroll interface
         var scrollCounterEthalon = model.testData.length;
         var scrollCounter = 0;
@@ -197,18 +198,22 @@ function ModelAPI() {
         else
             P.Logger.info("scroll of type 7 passed");
         model.testData.onScrolled = null;
-        model.testData.scrollTo(model.testData[55]);
-        if (model.testData.cursor !== model.testData[55])
-            throw 'testData.scrollTo() test failed';
-        if (model.testData.cursorPos !== 56)
-            throw 'testData.scrollTo()/cursorPos test failed';
-        P.Logger.info('testData.scrollTo test passed');
+        if (expectData) {
+            model.testData.scrollTo(model.testData[55]);
+            if (model.testData.cursor !== model.testData[55])
+                throw 'testData.scrollTo() test failed';
+            if (model.testData.cursorPos !== 56)
+                throw 'testData.scrollTo()/cursorPos test failed';
+            P.Logger.info('testData.scrollTo test passed');
+        }else{
+            P.Logger.info('testData.scrollTo test skipped');
+        }
     }
 
     function testModify() {
         // crud
         model.testData.deleteAll();
-        if (model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
+        if (!model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
             throw 'testData.deleteAll() test failed';
         P.Logger.info('testData.deleteAll() test passed');
         model.testData.requery(function() {
@@ -216,54 +221,54 @@ function ModelAPI() {
                 model.testData.first();
                 model.testData.deleteRow();
             }
-            if (model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
+            if (!model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
                 throw 'testData.deleteRow() test failed';
             P.Logger.info('testData.deleteRow() test passed');
             model.testData.requery(function() {
                 for (var i = model.testData.length; i >= 1; i--) {
                     model.testData.deleteRow(i);
                 }
-                if (model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
+                if (!model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
                     throw 'testData.deleteRow(int) test failed';
                 P.Logger.info('testData.deleteRow(int) test passed');
                 model.testData.requery(function() {
                     for (var i = model.testData.length; i >= 1; i--) {
                         model.testData.deleteRow(model.testData[i - 1]);
                     }
-                    if (model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
+                    if (!model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
                         throw 'testData.deleteRow(Row) test failed';
                     P.Logger.info('testData.deleteRow(Row) test passed');
                     model.testData.requery(function() {
                         while (!model.testData.empty) {
                             model.testData.pop();
                         }
-                        if (model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
+                        if (!model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
                             throw 'testData.pop() test failed';
                         P.Logger.info('testData.pop() test passed');
                         model.testData.requery(function() {
                             while (model.testData.length > 0) {
                                 model.testData.shift();
                             }
-                            if (model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
+                            if (!model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
                                 throw 'testData.shift() test failed';
                             P.Logger.info('testData.shift() test passed');
                             model.testData.requery(function() {
                                 // insert/push/unshift tests                                
                                 model.testData.splice(0, model.testData.length);
-                                if (model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
+                                if (!model.testData.empty || model.testData.size > 0 || model.testData.length > 0)
                                     throw 'testData.splice(0, model.testData.length) test failed';
                                 P.Logger.info('testData.splice(0, model.testData.length) test passed');
                                 model.testData.push({MDENT_ID: 45, MDENT_NAME: 'sn', MDENT_TYPE: '50'});
                                 if (model.testData.length !== 1 || model.testData.size !== 1 || model.testData.empty)
                                     throw 'testData.push({...}) test failed 1';
-                                if (model.testData[0].MDENT_ID !== '45' || model.testData[0].MDENT_NAME !== 'sn' || model.testData[0].MDENT_TYPE !== 50)
+                                if (model.testData[0].MDENT_ID !== '45.0' || model.testData[0].MDENT_NAME !== 'sn' || model.testData[0].MDENT_TYPE !== 50)
                                     throw 'testData.push({...}) test failed 2';
                                 P.Logger.info('testData.push({...}) test passed');
                                 model.testData.pop();
                                 model.testData.unshift({MDENT_ID: 46, MDENT_NAME: 'sm', MDENT_TYPE: '55'});
                                 if (model.testData.length !== 1 || model.testData.size !== 1 || model.testData.empty)
                                     throw 'testData.unshift({...}) test failed 1';
-                                if (model.testData[0].MDENT_ID !== '46' || model.testData[0].MDENT_NAME !== 'sm' || model.testData[0].MDENT_TYPE !== 55)
+                                if (model.testData[0].MDENT_ID !== '46.0' || model.testData[0].MDENT_NAME !== 'sm' || model.testData[0].MDENT_TYPE !== 55)
                                     throw 'testData.unshift({...}) test failed 2';
                                 P.Logger.info('testData.unshift({...}) test passed');
                                 model.testData.shift();
@@ -277,26 +282,26 @@ function ModelAPI() {
                                 model.testData.insert(1, 42, model.testData.schema.MDENT_NAME, 'sk', 3, '60');
                                 if (model.testData.length !== 1 || model.testData.size !== 1 || model.testData.empty)
                                     throw 'testData.insert(...) test failed 1';
-                                if (model.testData[0].MDENT_ID !== '42' || model.testData[0].MDENT_NAME !== 'sk' || model.testData[0].MDENT_TYPE !== 60)
+                                if (model.testData[0].MDENT_ID !== '42.0' || model.testData[0].MDENT_NAME !== 'sk' || model.testData[0].MDENT_TYPE !== 60)
                                     throw 'testData.insert(...) test failed 2';
                                 P.Logger.info('testData.insert(...) test passed');
                                 model.testData.splice(0, 0, {MDENT_ID: 43, MDENT_NAME: 'ss', MDENT_TYPE: '65'});
                                 if (model.testData.length !== 2 || model.testData.size !== 2 || model.testData.empty)
                                     throw 'testData.splice(...) test failed 1';
-                                if (model.testData[0].MDENT_ID !== '43' || model.testData[0].MDENT_NAME !== 'ss' || model.testData[0].MDENT_TYPE !== 65)
+                                if (model.testData[0].MDENT_ID !== '43.0' || model.testData[0].MDENT_NAME !== 'ss' || model.testData[0].MDENT_TYPE !== 65)
                                     throw 'testData.splice(...) test failed 2';
                                 P.Logger.info('testData.splice(...) test passed');
                                 model.testData.insertAt(3, 1, 44, model.testData.schema.MDENT_NAME, 'sp', 3, '66');
-                                if (model.testData.length !== 3 || model.testData.size !== 2 || model.testData.empty)
+                                if (model.testData.length !== 3 || model.testData.size !== 3 || model.testData.empty)
                                     throw 'testData.insertAt(...) test failed 1';
-                                if (model.testData[2].MDENT_ID !== '44' || model.testData[2].MDENT_NAME !== 'sp' || model.testData[2].MDENT_TYPE !== 66)
+                                if (model.testData[2].MDENT_ID !== '44.0' || model.testData[2].MDENT_NAME !== 'sp' || model.testData[2].MDENT_TYPE !== 66)
                                     throw 'testData.insertAt(...) test failed 2';
                                 P.Logger.info('testData.insertAt(...) test passed');
                                 model.testData.last();
                                 model.testData.cursor.MDENT_ID = 50;
                                 model.testData.cursor.MDENT_NAME = 'sf';
                                 model.testData.cursor.MDENT_TYPE = 70;
-                                if (model.testData[1].MDENT_ID !== '50' || model.testData[1].MDENT_NAME !== 'sf' || model.testData[1].MDENT_TYPE !== 70)
+                                if (model.testData[2].MDENT_ID !== '50.0' || model.testData[2].MDENT_NAME !== 'sf' || model.testData[2].MDENT_TYPE !== 70)
                                     throw 'testData.cursor.<...> = test failed';
                                 P.Logger.info('testData.cursor.<...> = test passed');
                                 model.testData[0].MDENT_ID = '60';
@@ -324,7 +329,11 @@ function ModelAPI() {
         model.testData.requery(function() {
             var unfilteredLength = model.testData.length;
             var filter = model.testData.createFilter(model.testData.schema.MDENT_TYPE);
-            filter.apply(70);
+            filter.apply(70, true, 67);
+            if(model.testData.empty)
+                throw 'filter no content failed.';
+            if (model.testData.activeFilter !== filter)
+                throw 'filter/activeFilter test failed';
             var filteredLength = model.testData.length;
             if (filteredLength >= unfilteredLength)
                 throw 'filter test failed';
@@ -344,7 +353,26 @@ function ModelAPI() {
             if (unfilteredLength1 !== unfilteredLength)
                 throw 'filter.cancel test failed';
             P.Logger.info('filter test passed');
-            model.testData.cre
+            var sorter = model.testData.createSorting(model.testData.schema.MDENT_TYPE, true, model.testData.schema.MDENT_NAME, false);
+            model.testData.sort(sorter);
+            if (model.testData[0].MDENT_TYPE !== 70)
+                throw 'testData.sort() test failed 1';
+            model.testData.sort(function(o1, o2) {
+                return o2.MDENT_TYPE - o1.MDENT_TYPE;
+            });
+            if (model.testData[0].MDENT_TYPE !== 90)
+                throw 'testData.sort() test failed 2';
+            P.Logger.info('testData.sort() test passed');
+            var modules = model.testData.find(model.testData.schema.MDENT_TYPE, 20);
+            if (!modules || !Array.isArray(modules) || modules.length === 0 || !modules[2])
+                throw 'testData.find test failed';
+            P.Logger.info('testData.find test passed');
+            var module2 = model.testData.findById(modules[2].MDENT_ID);
+            if (!module2 || module2 !== modules[2])
+                throw 'testData.findById test failed';
+            P.Logger.info('testData.findById test passed');
+            model.testData.execute();
+            P.Logger.info('testData.execute() test passed');
         });
     }
 
@@ -352,16 +380,22 @@ function ModelAPI() {
         P.Logger.info("=== test on empty data ===");
         testArrayLike();
         testScroll();
+        if (model.testData.params.Param1 === undefined)
+            throw 'testData.params test failed 1';
+        model.testData.params.Param1 = 89;
+        if (model.testData.params.Param1 !== 89)
+            throw 'testData.params test failed 2';
+        P.Logger.info('testData.params test passed');
+
         model.testData.onRequeried = function(event) {
             requeried = true;
         };
-
         model.requery(function() {
             P.Logger.info("=== test on full data ===");
             if (model.testData.empty)
                 throw 'Data for tests missing';
             testArrayLike();
-            testScroll();
+            testScroll(true);
             testModify();
             if (!requeried)
                 throw 'onRequeried event missed';
